231098bc70879f22866f402e3abafc69855ea581
gingerails <rocketcityabi@gmail.com>
2022-11-20 23:29:53 -0600

    Initial commit

---
+++ lib/Ruby_Prog_Cs424.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Ruby_Prog_Cs424/version"
+
+module RubyProgCs424
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "Ruby_Prog_Cs424"
+
+require "minitest/autorun"
